f5e8edc6cddbd859237ad6aa59228287a132ad8f
Elias Rudberg <eli.rud@hotmail.com>
2019-10-10 15:32:44 +0200

    Adds working project 2 with parts A and B. Also adds some notes for project 1

--- project_1/test.py
+++ project_1/test.py
@@ -25,14 +25,14 @@ for j in range(21):
     print(j+1)
     entry = datahex[19456+64*j:19520+64*j]
     print(len(entry))
-    print(f'Filename: {unfuck(entry[0:16])}\nExtension: {unfuck(entry[16:22])}\nAttributes: {unfuck(entry[22:24])}\n'
+    print(f'Filename: {unfuck(entry[0:16])} - {"".join(reversed(bytes.fromhex(unfuck(entry[0:16])).decode("utf-8")))}\nExtension: {unfuck(entry[16:22])}\nAttributes: {unfuck(entry[22:24])}\n'
           f'Creation Time: {unfuck(entry[28:32])}\nCreation Date: {unfuck(entry[32:36])}\n'
           f'Last Access Date: {unfuck(entry[36:40])}\nLast Write Time: {unfuck(entry[44:48])}\n'
           f'Last Write Date: {unfuck(entry[48:52])}\nFirst Logical Cluster: {unfuck(entry[52:56])}\n'
           f'File Size: {unfuck(entry[56:])}\n..................................')
 
 
-print(datahex.find(unfuck("04034b50").upper()))
-f = open("test.zip", "wb")
-f.write(data[148992:])
-f.close()
+# print(datahex.find(unfuck("04034b50").upper()))
+# f = open("test.zip", "wb")
+# f.write(data[148992:])
+# f.close()
--- project_2/client.py
+++ project_2/client.py
@@ -1,12 +1,21 @@
 import socket
-
+import base64
+from cryptography.fernet import Fernet
 from cryptography.hazmat.backends import default_backend
 from cryptography.hazmat.primitives import hashes
 from cryptography.hazmat.primitives.asymmetric import ec
 from cryptography.hazmat.primitives.kdf.hkdf import HKDF
 from cryptography.hazmat.primitives.serialization import Encoding, PublicFormat
 
-serveraddress = ('127.0.0.1', 10000)
+#TODO
+# Implement authentication/verification using PSK.
+# Implement nounce to stop replay attacks.
+# Make sure data transfer does not exceed 64bytes per package
+
+serveraddress = ('127.0.0.1', 10001)
+
+#PSK = b'd7xmxmydueRaiHTiEaS0pa8gsGnhgNJXMR82NWE_cbo='
+
 
 # Writes message in bytes
 message = b'VARSTRANGFARINTEVARALANGREAN64BYTES_____________________________'
@@ -26,6 +35,7 @@ public_key_in_bytes = public_key.public_bytes(Encoding.X962, PublicFormat.Compre
 
 try:
     # Send client public key to server
+    print('_______________________________________')
     print ('sending public key')
     print('_______________________________________')
     sent = clientsocket.sendto(public_key_in_bytes, serveraddress)
@@ -33,18 +43,55 @@ try:
     # Receive server public key
     print ('waiting to receive')
     print('_______________________________________')
-    server_public_key, server = clientsocket.recvfrom(512)
+    server_public_key, server = clientsocket.recvfrom(100)
     print ('received public key from server')
     print('_______________________________________')
     # Prints public key neatly
-    print('Server public key {}'.format(server_public_key))
+    print('Server public key {}\n'.format(server_public_key))
     print('_______________________________________')
     # New exchange must be done for each new session to achieve forward secrecy
-    #shared_key = private_key.exchange(ec.ECDH(), server_public_key)
     shared_key = private_key.exchange(ec.ECDH(), ec.EllipticCurvePublicKey.from_encoded_point(ec.SECP384R1(), server_public_key))
     print('Computed shared key:\n {}'.format(shared_key))
     print('_______________________________________')
+    # We want to derive a key from our shared key to destroy any structure that may be present
+    # We set the keylength to be 32bytes to be able to use Fernet to encrypt/decrypt later
+    # HKDF = HMAC key derivation function
+    derived_key = HKDF(
+        algorithm=hashes.SHA256(),
+        length=32,
+        salt=None,
+        info=b'encryption key',
+        backend=default_backend()
+    ).derive(shared_key)
+
+    print('Derived key: \n {}'.format(derived_key))
+    print('_______________________________________')
+
+    f = Fernet(base64.urlsafe_b64encode(derived_key))
 
 finally:
-    print ("closing socket")
+    print("Sending request for data")
+    clientsocket.sendto("gif data please".encode(), serveraddress)
+    print("waiting to recieve")
+    data = clientsocket.recvfrom(100)[0]
+    data = int(f.decrypt(data).decode())
+    print(f"Server has {data} packets in storage")
+
+    for i in range(data):
+        control = 0
+        encrypted_data_total = b''
+        while control == 0:
+            packet = clientsocket.recvfrom(100)[0]
+            head_list = [x for x in packet[0:4]]
+            encrypted_data_total = encrypted_data_total + packet[4:]
+
+            if len(packet) < 64:
+                control = 1
+        plaintext = f.decrypt(encrypted_data_total)
+        print('Received data from client: {}'.format(plaintext.decode()))
+
+
+        # print(f"Packet {i}: {packet}")
+
+    print("closing socket")
     clientsocket.close()
